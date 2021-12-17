276bbf4b70d65b5c0b889247fe973087dbc571c5
Vidhyathegreat <95758776+Vidhyathegreat@users.noreply.github.com>
2021-12-17 21:32:38 +0530

    Add files via upload

---
+++ p assigment3.py
@@ -0,0 +1,11 @@
+def message(a,b,c):
+    print(a+" "+b+" "+c+" ")
+message("welcome","to","python")
+
+
+
+
+def message(a,b,c,d):
+    print(a+" "+b+" "+c+" "+d+" ")
+message("successfully","commpled","the","assigment")
+    
