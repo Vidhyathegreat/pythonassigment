93165dcfd8bee3f1e07e820592212661cb43575e
Vidhyathegreat <95758776+Vidhyathegreat@users.noreply.github.com>
2021-12-17 16:44:40 +0530

    Add files via upload

---
+++ p assigment2.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter your choice"))
+
+if (c==1):
+    print("addition of the numbers:",a+b)
+elif(c==2):
+    print("subraction of the numbers:",a-b)
+else:
+    print("choose valid choice")
+    
+    
