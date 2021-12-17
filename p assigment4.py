ab1793ab836c7ed5fe9de2c72151f85fea6b44d8
Vidhyathegreat <95758776+Vidhyathegreat@users.noreply.github.com>
2021-12-17 22:02:52 +0530

    Add files via upload

---
+++ p assigment4.py
@@ -0,0 +1,24 @@
+#not operator#
+a=10
+if not(a%2==0 or a%5==0):
+    print("12 is not divisible by 2 or 5")
+else:
+    print("12 is divisible by either 2 or 5")
+
+
+#and operator#
+
+x=8
+print(x>6 and x<16)
+
+# or operator#
+
+a=1
+b=2
+if a or b:
+    print("alteast one number has boolean value as True")
+else:
+    print("all the number have boolean value as False")
+
+
+    
